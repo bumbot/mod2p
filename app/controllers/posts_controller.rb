c981c4ebb6f260f189bf62b35ccee28e4c0016cd
cjp34 <carlparm@gmail.com>
2020-02-28 15:59:57 -0500

    Post validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,13 @@ class PostsController < ApplicationController
     
     def create
         @post = Post.new(post_params)
-        byebug
+        if @post.valid?
+            @post.save
+            redirect_to post_path(@post)
+        else
+            render :new
+        end
         @post.save
-        redirect_to post_path(@post)
     end
 
     def edit
@@ -25,7 +29,11 @@ class PostsController < ApplicationController
     def update
         @post = Post.find(params[:id])
         @post.update(post_params)
-        redirect_to post_path(@post)
+        if @post.valid?
+            redirect_to post_path(@post)
+        else
+            render :edit
+        end
     end
 
     private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :post_favorites
+    validates :title, presence: true
+    validates :user_id, presence: true
+    validates :content, presence: true
 end
