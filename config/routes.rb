9aa7926e57dab2ee20fad71a9742c082de11f11b
cjp34 <carlparm@gmail.com>
2020-03-02 12:01:33 -0500

    Added Username to User Table, updated seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  get '/', to: "application#homepage"
   resources :users, only: [:index, :show, :edit, :update, :new, :create]
   resources :posts, only: [:index, :show, :edit, :update, :new, :create]
   resources :projects, only: [:index, :show, :edit, :update, :new, :create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_28_163449) do
+ActiveRecord::Schema.define(version: 2020_03_02_165614) do
 
   create_table "post_favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_02_28_163449) do
     t.date "cohort_start"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
