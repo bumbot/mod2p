951b8bc633271bc88d9644492041da67a00bb0d1
cjp34 <carlparm@gmail.com>
2020-03-04 14:15:25 -0500

    User Controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+        @user_posts = @user.user_posts
     end
 
     def edit
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.valid?
             @user.save
-            session[:user_id] = user.id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
